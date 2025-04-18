e5c29016401484d4854311f51a8e6d0f70f884ce
Weslei Augusto C <85812820+weslei573@users.noreply.github.com>
2025-04-18 16:20:58 -0400

    condicionais

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        String nome = "Loca";
+        double salario = 2890.0;
+        int numeroDependentes = 2;
+        boolean isento = true;
+        if (salario > 2259.20 && !isento) {
+            double irrf = salario / 100 * 7.5;
+            System.out.println("Valor irrf: " + irrf);
+        } else if (isento) {
+            System.out.println("Contribuinte isento de irrf.");
+        } else {
+            System.out.println("Não há valores de irrf.");
         }
     }
 }
